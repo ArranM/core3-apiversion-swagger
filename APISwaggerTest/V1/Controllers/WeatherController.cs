f77701b0e341a64fff661f16ab37d0424d4e39d8
Arran <arran@web-site.co.uk>
2020-08-07 16:31:56 +0100

    demo showing swagger and api versioning

---
+++ APISwaggerTest/V1/Controllers/WeatherController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Linq;
+
+namespace APISwaggerTest.V1.Controllers
+{
+    /// <summary>
+    /// Represents a RESTful weather service.
+    /// </summary>
+    [ApiController]
+    [ApiVersion("1")]
+    [Route("api/v{version:apiVersion}/[controller]")]
+    public class WeatherController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherController> _logger;
+
+        public WeatherController(ILogger<WeatherController> logger)
+        {
+            _logger = logger;
+        }
+
+        /// <summary>
+        /// Gets weather.
+        /// </summary>
+        /// <param name="id">The requested weather identifier.</param>
+        /// <returns>The requested weather forecast.</returns>
+        /// <response code="200">The weather was successfully retrieved.</response>
+        /// <response code="404">The waather does not exist.</response>
+        [HttpGet("{id:int}")]
+        [Produces("application/json")]
+        [ProducesResponseType(typeof(WeatherForecast), 200)]
+        [ProducesResponseType(404)]
+        public IActionResult Get(int id)
+        {
+            var rng = new Random();
+            var result = Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+
+            return Ok(result);
+        }
+    }
+}
---
+++ APISwaggerTest/V2/Controllers/WeatherController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace APISwaggerTest.V2.Controllers
+{
+    /// <summary>
+    /// Represents a RESTful weather service.
+    /// </summary>
+    [ApiController]
+    [ApiVersion("2")]
+    [ApiVersion("1", Deprecated = true)]
+    [Route("api/v{version:apiVersion}/[controller]")]
+    public class WeatherController : ControllerBase
+    {
+        private ILogger<WeatherController> _logger;
+
+        public WeatherController(ILogger<WeatherController> logger)
+        {
+            _logger = logger;
+        }
+
+        /// <summary>
+        /// Gets weather.
+        /// </summary>
+        /// <param name="id">The requested weather identifier.</param>
+        /// <returns>The requested weather forecast.</returns>
+        /// <response code="200">The weather was successfully retrieved.</response>
+        /// <response code="404">The weather does not exist.</response>
+        [HttpGet("{id:int}"), MapToApiVersion("2")]
+        [Produces("application/json")]
+        [ProducesResponseType(typeof(WeatherForecast), 200)]
+        [ProducesResponseType(404)]
+        public IActionResult Get(int id)
+        {
+            var result = "no weather today";
+
+            return Ok(result);
+        }
+    }
+}
